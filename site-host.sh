623e6538d2a5406e9633bd939e68dc620fc827b7
tsuereth <tsuereth@tsuereth.com>
2025-12-13 09:21:40 -0800

    I keep forgetting to commit these local script changes. - Optionally get data updates from IGDB when preparing the site content. (Off by default, requires a local credentials file.) - During a local site-build, rewrite content files. - Also during a local site-build, simulate the live host/origin; this helps diff the rendered pages against actual, live pages.

--- site-host.sh
+++ site-host.sh
@@ -7,4 +7,12 @@ if [ ! -f "$GLOGGENERATOR" ]; then
     dotnet build GlogGenerator/GlogGenerator.csproj
 fi
 
-$GLOGGENERATOR host -i . -t GlogGenerator/templates/
+IGDB_CLIENT_CREDSFILE=.igdb-client-credentials
+if [ -f "$IGDB_CLIENT_CREDSFILE" ]; then
+    IGDB_CLIENT_ID=$(cat "$IGDB_CLIENT_CREDSFILE" | cut -d ':' -f 1)
+    IGDB_CLIENT_SECRET=$(cat "$IGDB_CLIENT_CREDSFILE" | cut -d ':' -f 2)
+
+    #IGDB_UPDATE_OPTIONS="-u true --igdb-client-id \"$IGDB_CLIENT_ID\" --igdb-client-secret \"$IGDB_CLIENT_SECRET\""
+fi
+
+$GLOGGENERATOR host -i . -t GlogGenerator/templates/ $IGDB_UPDATE_OPTIONS
